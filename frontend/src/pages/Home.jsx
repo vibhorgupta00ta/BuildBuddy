8f8e139ce44f033705bf01bc844e6fda9569af72
vibhor <vibhorg62@gmail.com>
2026-05-24 23:40:17 +0530

    Arrow Change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,8 +45,7 @@ const Home = () => {
           className="flex flex-col sm:flex-row gap-6"
         >
           <Link to="/discover" className="btn-primary group">
-            Start Discovering 
-            <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
+            Start Discovering
           </Link>
           <Link to="/teams" className="btn-secondary group">
             <Users className="w-5 h-5 mr-2 group-hover:text-brand-accent transition-colors" />
